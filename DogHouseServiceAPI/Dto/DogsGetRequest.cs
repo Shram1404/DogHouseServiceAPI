d55edd1de68ba284514a3ac0d4fc34f1a9440a4c
Олег Шрам <shram.20030414@gmail.com>
2023-06-11 23:32:27 +0300

    fix

--- DogHouseServiceAPI/Dto/DogsGetRequest.cs
+++ DogHouseServiceAPI/Dto/DogsGetRequest.cs
@@ -1,11 +1,16 @@
-﻿namespace DogHouseServiceAPI.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogHouseServiceAPI.Dto
 {
     // Parameter object for the GetDogs method in the DogsController class
     public class DogsGetRequest
     {
         public string? Attribute { get; set; }
         public string? Order { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Page can not be less or equal zero")]
         public int PageNumber { get; set; } = 1;
+        [Range(1, int.MaxValue, ErrorMessage = "Page size can not be less or equal zero")]
         public int PageSize { get; set; } = 10;
     }
 }
